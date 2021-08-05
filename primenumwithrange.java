44e5ca243046b37d405c1850b6f8bda0f850dc5c
hrishikesh-mehtre <hrmhetre@gmail.com>
2021-08-05 23:35:16 +0530

    doen

---
+++ primenumwithrange.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public  class primenumwithrange {
+    
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int from=sc.nextInt();
+        int to=sc.nextInt();
+        int count=0;
+        for(int i=0;i<=)
+    }
+}
