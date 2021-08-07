416a2a1f9bd7b4c8c3acb6637ea3c223e4d0b028
hrishikesh-mehtre <hrmhetre@gmail.com>
2021-08-07 12:00:33 +0530

    done

---
+++ araminrange.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class araminrange {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int from=sc.nextInt();
+        int to=sc.nextInt();
+        
+
+    }
+}
